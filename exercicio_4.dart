5f470a338300f49f5142da6dc5439fc42a910447
barbara <barbara31fab@gmail.com>
2020-07-07 19:46:32 -0300

    getter e setter

--- exercicio_4.dart
+++ exercicio_4.dart
@@ -3,7 +3,7 @@ import 'pessoa.dart';
 main() {
   var p1 = Pessoa();
   p1.nome = 'Julius';
-  // p1._cpf = '123.876.543-12';
+  p1.cpf = '123.876.543-12';
 
   // print('O ${p1.nome} tem o CPF: ${p1._cpf}');
   print('O ${p1.nome}');
--- pessoa.dart
+++ pessoa.dart
@@ -3,4 +3,12 @@ import 'exercicio_4.dart';
 class Pessoa {
   String nome;
   String _cpf; //privado somente para arquivos externos
+
+  set cpf(String cpf) {
+    _cpf = cpf;
+  }
+
+  get cpf {
+    return _cpf;
+  }
 }
