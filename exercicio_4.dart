f2ef0b6e24341eba845dba8bb504158b29b57517
barbara <barbara31fab@gmail.com>
2020-07-07 19:37:34 -0300

    exemplo visibilidade - 2

--- exercicio_4.dart
+++ exercicio_4.dart
@@ -1,12 +1,10 @@
-class Pessoa {
-  String nome;
-  String _cpf; //privado somente para arquivos externos
-}
+import 'pessoa.dart';
 
 main() {
   var p1 = Pessoa();
   p1.nome = 'Julius';
-  p1._cpf = '123.876.543-12';
+  // p1._cpf = '123.876.543-12';
 
-  print('O ${p1.nome} tem o CPF: ${p1._cpf}');
+  // print('O ${p1.nome} tem o CPF: ${p1._cpf}');
+  print('O ${p1.nome}');
 }
---
+++ pessoa.dart
@@ -0,0 +1,6 @@
+import 'exercicio_4.dart';
+
+class Pessoa {
+  String nome;
+  String _cpf; //privado somente para arquivos externos
+}
